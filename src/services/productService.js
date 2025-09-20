5417e55bcfcec22300ad294d1f0c2893621a2e1a
JangaSrithaReddy <srithajanga5877@gmail.com>
2025-09-20 08:01:17 +0530

    Update productService.js

--- src/services/productService.js
+++ src/services/productService.js
@@ -1,11 +1,11 @@
 import axios from "axios";
 
-const API_BASE_URL = "http://localhost:9090/back1/api/products";  // Updated Backend API URL
+const API_BASE_URL = "http://34.224.212.12:8081/api/products";  // Backend API URL
 
 // Fetch products by category (computers, mobiles, laptops, pendrives, or all)
 export const getProducts = async (category = "") => {
   try {
-    const response = await axios.get(`${API_BASE_URL}/${category}`);
+    const response = await axios.get(${API_BASE_URL}/${category});
     return response.data;  // Return products array
   } catch (error) {
     console.error("Error fetching products:", error);
@@ -15,5 +15,5 @@ export const getProducts = async (category = "") => {
 
 // Get product image URL
 export const getProductImageUrl = (imagePath) => {
-  return `${API_BASE_URL}/images/${imagePath}`;
+  return `http://localhost:8080/api/products/images/${imagePath}`;
 };
